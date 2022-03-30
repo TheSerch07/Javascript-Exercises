889c8992f78572e07e35cad7ebe543c61dbd5846
TheSerch07 <jejog50@gmail.com>
2022-03-30 15:23:33 -0500

    ejercicio5

--- ejercicios5js.js
+++ ejercicios5js.js
@@ -1,3 +1,10 @@
 // 15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.
 // 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
 // 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
+
+function binaryToDecimal(number, base) {
+    if (typeof number !== "number" || typeof base !== "number") return null
+    else return parseInt(number, base)
+}
+
+console.log(binaryToDecimal(100, 2))
